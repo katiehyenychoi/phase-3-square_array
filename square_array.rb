1431b93eb044c84f6908a3c011f7a22187518c0f
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-05-16 11:38:36 -0400

    commit tree c893959d1f8517cb6baca27662c23b74424d25f9

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+# def square_array(array)
+#     array.each do |num|
+#     num = num^2
+#       end
+# end
+
 def square_array(array)
-  # your code here
+  squared = []
+  array.each{|num| squared << num*num}
+  squared
 end
+
+square_array([1,2,3])
